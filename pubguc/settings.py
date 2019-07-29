533891b0b8866cb5ea69f41ec0568ae6ebb619e2
gogo <praveennagil15821@gmail.com>
2019-07-29 12:36:17 +0530

    static root added

--- pubguc/settings.py
+++ pubguc/settings.py
@@ -121,8 +121,10 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+
+
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
